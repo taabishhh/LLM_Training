73c11f41e25b776fa52975b341b193a0a0ecb21c
taabish10s <tsutr@uic.edu>
2024-11-03 22:02:31 -0600

    test cases and logging added

--- src/main/scala/TransformerModel.scala
+++ src/main/scala/TransformerModel.scala
@@ -3,28 +3,26 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.nd4j.linalg.activations.Activation
 import org.nd4j.linalg.learning.config.Adam
 
+
 object TransformerModel {
-  // Method to create and initialize a simple neural network model
   def createModel(inputSize: Int, hiddenSize: Int, outputSize: Int): MultiLayerNetwork = {
-
-    // Define the model configuration
     val conf: MultiLayerConfiguration = new NeuralNetConfiguration.Builder()
-      // Set the optimizer for the model to Adam with a learning rate of 0.001
       .updater(new Adam(0.001))
-      .list() // Start building a list of layers for a sequential model
-
-      // Add the first dense layer (hidden layer) with ReLU activation
+      .list()
       .layer(0, new org.deeplearning4j.nn.conf.layers.DenseLayer.Builder()
-        .nIn(inputSize) // Number of input nodes (features)
-        .nOut(hiddenSize) // Number of output nodes in the hidden layer
-        .activation(Activation.RELU) // Activation function for hidden layer
+        .nIn(inputSize)
+        .nOut(hiddenSize)
+        .activation(Activation.RELU)
         .build())
-
-      // Add the output layer with Softmax activation for classification tasks
       .layer(1, new org.deeplearning4j.nn.conf.layers.OutputLayer.Builder()
-        .nIn(hiddenSize) // Number of input nodes from the hidden layer
-        .nOut(outputSize) // Number of output nodes (classes)
-        .activation(Activation.SOFTMAX) // Activation function for output layer
+        .nIn(hiddenSize)
+        .nOut(outputSize)
+        .activation(Activation.SOFTMAX)
         .build())
+      .build()
 
-// Build the configuration for the
+    val model = new MultiLayerNetwork(conf)
+    model.init()
+    model
+  }
+}
